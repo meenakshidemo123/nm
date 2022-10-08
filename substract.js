16b6dcbb85d0d7ed65ae8f61fb6cfdd8859fc769
meenakshi <meenakshipaluchuri@gmail.com>
2022-10-08 08:29:49 -0700

    substract.js

---
+++ substract.js
@@ -0,0 +1,7 @@
+function substract() {
+var a = document.getElementsById("firstno").value
+var b = document.getElementsById("secondno").value
+var result
+result = a-b
+document.getElementsById("result").value=result
+}
